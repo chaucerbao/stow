6f2942d254a2f9fd5aa4551f2bedabcdf980e8be
Alvin Chan <aschan@gmail.com>
2018-03-29 21:57:16 -0700

    Add identity tests

--- src/stow.test.ts
+++ src/stow.test.ts
@@ -18,9 +18,11 @@ test('new Stow()', t => {
 test('.get(id)', t => {
   const { stow } = t.context
 
-  const model = stow.get(1)
+  const modelA = stow.get(1)
+  const modelB = stow.get(1)
 
-  t.is(model.name, 'Initial name')
+  t.is(modelA.name, 'Initial name')
+  t.is(modelA, modelB)
 })
 
 test('.set(id)', t => {
@@ -37,15 +39,20 @@ test('.set(id, props)', t => {
   const model = stow.set(1, { name: 'Ken Masters' })
 
   t.is(model.name, 'Ken Masters')
+
+  stow.set(1, { name: 'Ryu Hoshi' })
+
+  t.is(model.name, 'Ryu Hoshi')
 })
 
 test('.set(id, props) then .get(id)', t => {
   const { stow } = t.context
 
-  stow.set(1, { name: 'Ken Masters' })
-  const model = stow.get(1)
+  const modelA = stow.set(1, { name: 'Ken Masters' })
+  const modelB = stow.get(1)
 
-  t.is(model.name, 'Ken Masters')
+  t.is(modelB.name, 'Ken Masters')
+  t.is(modelA, modelB)
 })
 
 test('.set(id, props) with extra props', t => {
